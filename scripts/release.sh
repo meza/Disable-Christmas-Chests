86f6ccb0514a31882799421fd48e102386a6b16b
meza <211977+meza@users.noreply.github.com>
2024-12-23 11:16:35 +0000

    ci: using chiseledbuild

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,4 +5,4 @@ VERSION=$1
 echo "Replacing version with ${VERSION}"
 sed -e "s/0.0-SNAPSHOT/${VERSION}/" -i gradle.properties
 
-./gradlew build -x test # we've ran the tests earlier in the build
+./gradlew chiseledBuild
