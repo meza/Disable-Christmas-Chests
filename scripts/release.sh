816dc44f672d7079c1ea0c95e8fcfd351ebaaf58
meza <211977+meza@users.noreply.github.com>
2024-12-28 12:20:20 +0000

    ci: better build settings

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,4 +5,4 @@ VERSION=$1
 echo "Replacing version with ${VERSION}"
 sed -e "s/0.0-SNAPSHOT/${VERSION}/" -i gradle.properties
 
-./gradlew chiseledBuildAndCollect chiseledPublishMods
+./gradlew chiseledBuildAndCollect chiseledPublishMods --no-daemon
